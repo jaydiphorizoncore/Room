1e9ac90e11bf74b99833693605b69fef95de701d
jaydiphorizoncore <jaydip.jadav@horizoncore.in>
2023-09-22 10:01:02 +0530

    initial

---
+++ app/build.gradle.kts
@@ -0,0 +1,70 @@
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.room"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.room"
+        minSdk = 24
+        targetSdk = 33
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+}
+
+dependencies {
+
+    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.9.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("androidx.room:room-common:2.5.2")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    // Room components
+
+    implementation("androidx.room:room-runtime:2.6.0-beta01")
+    kapt("androidx.room:room-compiler:2.6.0-beta01")
+    implementation("androidx.room:room-ktx:2.6.0-beta01")
+
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    // LiveData
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    // Lifecycles only (without ViewModel or LiveData)
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
+
+    //Kotlin
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.10")
+
+    //coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+
+}
---
+++ app/src/main/java/com/example/room/NotesDao.kt
@@ -0,0 +1,27 @@
+package com.example.room
+
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.Query
+import androidx.room.Update
+
+@Dao
+interface NotesDao {
+    // adding a new entry to our database.
+    @Insert
+    suspend fun insert(note: Note)
+
+    // for deleting our note.
+    @Delete
+    suspend fun delete(note: Note)
+
+    // we have to get the data.
+    @Query("Select * from newTable order by id ASC")
+    fun getAllNotes(): LiveData<List<Note>>
+
+    // below method is use to update the note.
+    @Update
+    suspend fun update(note: Note)
+}
